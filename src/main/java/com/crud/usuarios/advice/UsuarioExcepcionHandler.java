631bfca1fab0e08f59ff23edb783334370a84cc6
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-04-12 23:23:32 -0400

    1)Usuario.java: se agregan validaciones para evitar nulos, control de largo de caracteres, email valido, contraseña con largo especifico de caracteres y que contenga letras y numeros. 2)Se implementa nueva clase que captura los errores en el ingreso de datos y los retorna de una forma legible.

---
+++ src/main/java/com/crud/usuarios/advice/UsuarioExcepcionHandler.java
@@ -0,0 +1,21 @@
+package com.crud.usuarios.advice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+//Capturar errores
+@RestControllerAdvice
+public class UsuarioExcepcionHandler {
+    //Metodo que retorna errores
+    @ExceptionHandler(MethodArgumentNotValidException.class)//Cuando se produzca esta excepcion se dispara el método
+    public Map<String, String> handleInvalidArguments(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return errors;
+    }
+}
--- src/main/java/com/crud/usuarios/model/Usuario.java
+++ src/main/java/com/crud/usuarios/model/Usuario.java
@@ -8,8 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuario")
@@ -20,40 +23,45 @@ public class Usuario {
     private int idUsuario;
 
     @Column(name = "nombre")
-    @NotNull(message = "El campo nombre no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(min = 2, max = 100, message = "Debe tener entre 2 y 100 caracteres")
     private String nombre;
 
     @Column(name = "apellido_paterno")
-    @NotNull(message = "El campo apellido paterno no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(min = 2, max = 50, message = "Debe tener entre 2 y 100 caracteres")
     private String apellidoPaterno;
 
     @Column(name = "apellido_materno")
-    @NotNull(message = "El campo apellido materno no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(min = 2, max = 50, message = "Debe tener entre 2 y 100 caracteres")
     private String apellidoMaterno;
 
     @Column(name = "email")
-    @NotNull(message = "El campo email no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(max = 50, message = "Debe tener un máximo de 50 caracteres")
+    @Email(message = "El email debe tener un formato válido")
     private String email;
 
     @Column(name = "contrasena")
-    @NotNull(message = "El campo apellido paterno no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(min = 6, max = 20, message = "La contraseña debe contener entre 6 y 20 caracteres con al menos una letra y un número")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,20}$", message = "La contraseña debe contener entre 6 y 20 caracteres con al menos una letra y un número")
     private String contrasena;
 
     @Column(name = "telefono")
-    private int telefono;
+    @Size(min = 9, max = 9, message = "Debe tener 9 dígitos")
+    @Pattern(regexp = "\\d{9}", message = "Debe contener solo dígitos")  
+    private String telefono;
     
     @Column(name = "direccion")
-    @NotNull(message = "El campo direccion no puede ser nulo")
-    @NotBlank
+    @NotNull
+    @Size(min = 2, max = 255, message = "Debe tener entre 2 y 255 caracteres")
     private String direccion;
 
     @ManyToOne(targetEntity = Perfil.class)
     @JoinColumn(name = "id_perfil")
+    @NotNull
     private Perfil perfil;
 
     public int getIdUsuario() {
@@ -80,7 +88,7 @@ public class Usuario {
         return contrasena;
     }
 
-    public int getTelefono() {
+    public String getTelefono() {
         return telefono;
     }
 
@@ -116,7 +124,7 @@ public class Usuario {
         this.contrasena = contrasena;
     }
 
-    public void setTelefono(int telefono) {
+    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
