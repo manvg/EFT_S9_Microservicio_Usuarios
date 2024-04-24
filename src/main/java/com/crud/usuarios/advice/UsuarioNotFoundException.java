89731d467d59404a8165455dbc08ef01b98cee83
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-04-23 22:24:01 -0400

    Nueva clase para manejo de excepciones en que no se encuentra el usuario

---
+++ src/main/java/com/crud/usuarios/advice/UsuarioNotFoundException.java
@@ -0,0 +1,15 @@
+package com.crud.usuarios.advice;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UsuarioNotFoundException extends RuntimeException {
+    
+    public UsuarioNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
